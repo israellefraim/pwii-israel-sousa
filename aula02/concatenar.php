e768ad9968d1c8e0749b1e44ce6715c7d769f551
Israel Efraim <israell.efraim@gmail.com>
2025-03-25 16:31:01 -0300

    Cria um arquivo sobre concatenação

---
+++ aula02/concatenar.php
@@ -0,0 +1,13 @@
+<?php
+    ECHO "Hello World!<br>";
+    echo "Hello World!<br>";
+    EcHo "Hello World!<br>";
+
+    $nome = "Israel";
+    $sobrenome = "Sousa";
+    $numero = 7;
+
+    echo $nome . " lindo" . "<br>";
+    echo "${nome} <h1> ${sobrenome} </h1>";
+    echo $numero . "<p> " . $nome . " " . $sobrenome . "</p>";
+?>
